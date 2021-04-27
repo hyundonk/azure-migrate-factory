7f0f266af550ddff6e45b6e6350d1a2e2b37e0d5
hyuk <calvin75@gmail.com>
2021-04-27 12:34:51 +0000

    Initial commit

---
+++ createimage.ps1
@@ -0,0 +1,41 @@
+# createimage_n_upload.ps1
+# created by hyuk@microsoft.com
+# input cvs file has list of "sysprep"ed VMs. Use this powershell script For each "sysprep"ed VM to generalized create image, and upload it to Shared Image Gallery as Image definition
+
+
+Param (
+    [Parameter(Mandatory=$true)]
+    $inputfile
+)
+
+$VMs = Import-CSV $inputfile 
+
+$counter = 0
+Foreach ($VM in $VMs){
+
+    $rg = Get-AzResourceGroup -Name $VM.RESOURCE_GROUP_VM
+    $vmname = $VM.VMNAME
+
+    Write-Host "[$vmname] Stopping ..."
+    Stop-AzVM  -ResourceGroupName $VM.RESOURCE_GROUP_VM -Name $vmname -Force
+
+    Write-Host "[$vmname] Generalizing ..."
+    Set-AzVM  -ResourceGroupName $VM.RESOURCE_GROUP_VM  -Name $vmname -Generalized
+
+    $vmObj = Get-AzVM -Name $vmname -ResourceGroupName $VM.RESOURCE_GROUP_VM
+
+    # Create the VM image configuration based on the source VM
+    $image = New-AzImageConfig -Location $rg.Location -SourceVirtualMachineId $vmObj.ID 
+
+    # Remove last 6 characters(p-test)
+    $length = $vmname.length
+    $imageName = $vmname.substring(0, $length - 6) + "Z"
+
+    Write-Host "[$vmname] Create Azure Image[$imageName]"
+    $myImage = New-AzImage -Image $image -ImageName $imageName -ResourceGroupName $VM.RESOURCE_GROUP_IMAGE
+
+    Write-Host "[$vmname] Image[$imageName] creation completed."
+    $counter++
+}
+
+Write-Host "Total $counter Images creation completed."
